1df3c7424c6f2e84d7e34280d3d2ad8a07d02e5f
Natasha Shireen Williams <natwill9876@gmail.com>
2026-03-27 17:38:26 +0500

    Add permutation function

---
+++ permutation.py
@@ -0,0 +1,15 @@
+def permutations(n):
+    
+    if len(n) == 0:
+        return ['']
+    hey = permutations(n[1:])
+    result =[]
+    for i in hey:
+        for p in range(len(i)+1):
+            perm = i[:p] +n[0] + i[p:]
+            print(perm)
+            result.append(perm)
+        
+    return result   
+
+print(permutations('abc'))  
